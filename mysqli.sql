d5ba029d2f101062bddcbf07232957f67996854d
Ung Hung <cusn1802@gmail.com>
2022-09-12 12:17:35 +0700

    edit database

--- mysqli.sql
+++ mysqli.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 12, 2022 at 07:09 AM
+-- Generation Time: Sep 12, 2022 at 07:16 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -42,49 +42,6 @@ CREATE TABLE `member` (
 INSERT INTO `member` (`id`, `username`, `password`, `phone`, `email`) VALUES
 (1, 'admin', '25d55ad283aa400af464c76d713c07ad', '123456789', 'cusn1802@gmail.com');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `created_time` int(11) NOT NULL,
-  `last_updated` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`, `created_time`, `last_updated`) VALUES
-(1, 'admin', '25d55ad283aa400af464c76d713c07ad', 123, 1553185530);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `ID` int(10) UNSIGNED NOT NULL,
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`ID`, `username`, `password`) VALUES
-(2, 'admin', 'admin'),
-(3, 'ronglon182', 'bichdiep1'),
-(4, 'ronglon182', 'bichdiep1'),
-(5, 'a', 'a');
-
 --
 -- Indexes for dumped tables
 --
@@ -96,18 +53,6 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -116,18 +61,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `member`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
